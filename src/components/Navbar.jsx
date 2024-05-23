2fabf070c679dbeeae29acc5fbf6d655844195ca
AYUSH SAHU <ayushsahu9803@gmail.com>
2024-05-23 16:11:11 +0530

    Updated Navbar.jsx with Org and Project info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 function Navbar() {
   
   return (
-    <div className='selection:bg-transparent selection:text-slate-100 z-[999] w-full h-20 px-32  flex justify-between items-center'>
-        <div className='logo h-full flex items-center'>
-            <img className='h-[65px]' src="/src/assets/website.png" alt="" />
-        </div>
+    <div className='selection:bg-transparent selection:text-slate-100 z-[999] w-full h-20 px-20 pt-12'>
+        
 
-        <div className='text-[#AD88C6] links flex gap-24'>
-            <a className='font-saira font-light capitalize text-2xl' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024">GSoC'24 OFFICIAL SITE</a>
-            <a className='mr-40 font-saira font-light capitalize text-2xl' target="_blanc" href="https://github.com/Murdock9803">MY GITHUB</a>
+        <div className='text-[#AD88C6] links flex justify-between gap-[10vw]'>
+            <a className='font-saira uppercase font-semibold tracking-light leading-none capitalize text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/organizations/creative-commons">Org: Creative Commons</a>
+            <a className='font-saira uppercase font-semibold tracking-light leading-none capitalize text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/projects/ZIhUmKRN">Project: Modernize CC Resource Archive</a>
         </div>
     </div>
   )
