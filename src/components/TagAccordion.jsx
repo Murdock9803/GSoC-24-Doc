0e24bcfa4dbbc3b57c248cc1138359a0791c6bc8
AYUSH SAHU <ayushsahu9803@gmail.com>
2024-05-23 18:17:03 +0530

    Added TagAccordion.jsx file

---
+++ src/components/TagAccordion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TagAccordion = (props) => {
+  return (
+    <div className="mt-20 uppercase text-[25px] font-saira font-bold tracking-wide bg-gray-800 text-sky-300 h-fit w-fit px-8 py-3 mb-4 rounded-lg">{props.title}</div>
+  )
+}
+
+export default TagAccordion
