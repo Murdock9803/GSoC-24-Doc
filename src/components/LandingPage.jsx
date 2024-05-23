0aa540ee9a41fe71f1b7abf9417ee6a603dc39b9
AYUSH SAHU <ayushsahu9803@gmail.com>
2024-05-23 16:08:57 +0530

    Updated LandingPage.jsx with mentor and contributor info

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage() {
                     initial={{ width: 0 }}
                     animate={{ width: "9vw" }}
                     transition={{ ease: [0.76, 0, 0.24, 1], duration: 1.5 }}
-                    className='mr-[1vw] w-[9vw] rounded-md h-[5.5vw] relative top-0 bg-red-500 bg-[url("/src/assets/gsoc.png")] bg-cover bg-center'
+                    className='mr-[1vw] ml-[0.35vw] w-[9vw] rounded-md h-[5.5vw] relative top-0 bg-red-500 bg-[url("/src/assets/gsoc.png")] bg-cover bg-center'
                   ></motion.div>
                 )}
                 <h1 className="text-[7.5vw] font-saira font-bold tracking-tight leading-[1]">
@@ -28,9 +28,9 @@ function LandingPage() {
           );
         })}
       </div>
-      <div className="border-t-[2px] text-[#AD88C6] text-[1.1vw] border-[#AD88C6] mt-20 flex justify-between items-center py-5 px-20">
-        <a target="_blank" href="https://summerofcode.withgoogle.com/programs/2024/organizations/creative-commons" className=" uppercase font-semibold tracking-light leading-none">Org: Creative Commons</a>
-        <a target="_blank" href="https://summerofcode.withgoogle.com/programs/2024/projects/ZIhUmKRN" className=" uppercase font-semibold tracking-light leading-none">Project: Modernize CC Resource Archive</a>
+      <div className="font-saira border-t-[2px] text-[#AD88C6] text-[1.5vw] border-[#AD88C6] mt-20 flex justify-between items-center py-5 px-20">
+        <a target="_blank" href="https://github.com/possumbilities" className=" uppercase font-semibold tracking-light leading-none">Mentor : Sara Lovell (Possumbilities)</a>
+        <a target="_blank" href="https://github.com/Murdock9803" className=" uppercase font-semibold tracking-light leading-none">Contributer : Ayush Sahu (Murdock9803)</a>
  
         <div className="relative start flex items-center gap-1 rounded-full" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
           <a
@@ -38,12 +38,12 @@ function LandingPage() {
             className="absolute w-full h-full rounded-full z-[1]"
             href="https://drive.google.com/file/d/1Qr8k1Q-PhjAOvfxfasAJ-lAiRakFuUWB/view?usp=drive_link"></a>
           <div
-            className={` ${isHovered ? "bg-[#000] text-[#fff] border-[#000000] transition-colors duration-300" : "bg-[#AD88C6] text-[#000000] transition-colors duration-300"} px-8 py-2 font-semibold uppercase rounded-full`}
+            className={` ${isHovered ? "bg-[#000] text-[#fff] border-[#000000] transition-colors duration-300" : "bg-[#AD88C6] text-[#000000] transition-colors duration-300"} px-[2vw] py-[0.5vw] font-semibold uppercase rounded-full`}
           >
             My Project Proposal
           </div>
           <div
-            className={` ${isHovered ? "bg-[#000] text-[#fff] border-[#000000] transition-colors duration-300" : "bg-[#AD88C6] text-[#000000] transition-colors duration-300"} w-10 h-10 flex items-center justify-center rounded-full`}
+            className={` ${isHovered ? "bg-[#000] text-[#fff] border-[#000000] transition-colors duration-300" : "bg-[#AD88C6] text-[#000000] transition-colors duration-300"} w-[3vw] h-[3vw] flex items-center justify-center rounded-full`}
           >
             <span className="rotate-[-45deg]">
               <FaArrowRight />
