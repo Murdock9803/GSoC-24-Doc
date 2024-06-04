dcf2b72843f367880c061955877e31371c05fdcb
AYUSH SAHU <ayushsahu9803@gmail.com>
2024-06-04 11:24:48 +0530

    Added responsiveness to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Navbar() {
   
   return (
-    <div className='selection:bg-transparent selection:text-slate-100 z-[999] w-full h-20 px-20 pt-12'>
+    <div className='selection:bg-transparent selection:text-slate-100 z-[999] w-full h-20 px-8 sm:px-20 pt-8 sm:pt-12'>
         
 
-        <div className='text-[#AD88C6] links flex justify-between gap-[10vw]'>
-            <a className='font-saira uppercase font-semibold tracking-light leading-none hover:underline capitalize text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/organizations/creative-commons">Org: Creative Commons</a>
-            <a className='font-saira uppercase font-semibold tracking-light leading-none hover:underline capitalize text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/projects/ZIhUmKRN">Project: Modernize CC Resource Archive</a>
+        <div className='text-[#AD88C6] links flex flex-col sm:flex-row gap-4 justify-between'>
+            <a className='font-saira uppercase font-semibold tracking-light leading-none hover:underline text-[11px] sm:text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/organizations/creative-commons">Org: Creative Commons</a>
+            <a className='font-saira uppercase font-semibold tracking-light leading-none hover:underline text-[11px] sm:text-[1.5vw]' target="_blanc" href="https://summerofcode.withgoogle.com/programs/2024/projects/ZIhUmKRN">Project: Modernize CC Resource Archive</a>
         </div>
     </div>
   )
